07d6707470ed2fa47abd35e066d265d8693e237c
nguyenan1997 <nguyenan14081997@gmail.com>
2026-02-13 17:46:34 +0700

    feat: add API configuration for user authentication and spin game endpoints.

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:3001/api/v1';
+export const API_BASE_URL = 'https://vivipp.mayogu.com/api/v1';
 
 export const API_ENDPOINTS = {
     LOGIN: `${API_BASE_URL}/users/login`,
