aa84b359c17454e7615b01a99289923e4fae1c3e
nguyenan1997 <nguyenan14081997@gmail.com>
2026-02-14 09:49:58 +0700

    feat: Introduce API configuration with base URL and specific endpoints.

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'https://vivipp.mayogu.com/api/v1';
-export const API_BASE_URL = 'http://localhost:3001/api/v1';
+export const API_BASE_URL = 'https://vivipp.mayogu.com/api/v1';
+// export const API_BASE_URL = 'http://localhost:3001/api/v1';
 
 export const API_ENDPOINTS = {
     LOGIN: `${API_BASE_URL}/users/login`,
